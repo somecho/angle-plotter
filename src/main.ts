5cefe1035be5768643b4bc125831bded3021dab9
Some Cho <itssomicho@gmail.com>
2024-06-15 11:08:20 +0200

    further work on UI
    - adjust rect size to 10
    - add contrasting elements for better text legibility
    - adjust DOM elements for basic formatting
    - change base font for UI
    - use offsetX instead of clientX so that canvas can be moved

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-const RECT_SIZE = 12;
+const RECT_SIZE = 10;
 let nodes: GraphNode[] = [];
 let edges: Edge[] = [];
+let img = new Image();
+const MAIN_COL = "#fa0a76"
 
 const canvas = document.createElement("canvas")
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
@@ -8,12 +10,31 @@ canvas.width = 720
 canvas.height = 480
 ctx.textBaseline = "middle"
 ctx.textAlign = "center"
-ctx.font = "12px Silkscreen"
+ctx.font = "14px Silkscreen"
 
-document.body.appendChild(canvas)
+document.querySelector("#app")?.appendChild(canvas)
 canvas.addEventListener('mouseup', onMouseUp)
 setup()
 
+document.querySelector("#upload-btn")?.addEventListener("change", (e) => {
+  const reader = new FileReader()
+  reader.addEventListener("load", (e) => {
+    img = new Image()
+    img.addEventListener("load", (e) => {
+      nodes = []
+      edges = []
+      canvas.width = img.width
+      canvas.height = img.height
+      ctx.textBaseline = "middle"
+      ctx.textAlign = "center"
+      ctx.font = "14px Silkscreen"
+      ctx.drawImage(img, 0, 0)
+    })
+    img.src = e.target?.result as string
+  })
+  const target = e.target as HTMLInputElement;
+  reader.readAsDataURL((target.files as FileList)[0])
+})
 
 function getSelectedNode(mouseX: number, mouseY: number): Id | undefined {
   for (let i = 0; i < nodes.length; i++) {
@@ -28,8 +49,10 @@ function getSelectedNode(mouseX: number, mouseY: number): Id | undefined {
 }
 
 function setup() {
-  ctx.fillStyle = "#11f9b9"
-  ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
+  if (img.width == 0 && img.height == 0) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+  }
+  ctx.drawImage(img, 0, 0)
 }
 
 function getLastActiveNodeIndex(): Id | undefined {
@@ -51,7 +74,7 @@ function findPivots() {
 }
 
 function onMouseUp(e: MouseEvent) {
-  const idx = getSelectedNode(e.clientX, e.clientY)
+  const idx = getSelectedNode(e.offsetX, e.offsetY)
   const lastActive = getLastActiveNodeIndex()
   nodes.forEach(node => node.active = false)
   if (idx == undefined) {
@@ -61,7 +84,7 @@ function onMouseUp(e: MouseEvent) {
         idxB: lastActive as Id,
       })
     }
-    nodes.push({ x: e.clientX, y: e.clientY, active: true })
+    nodes.push({ x: e.offsetX, y: e.offsetY, active: true })
   } else {
     const edgeExists = edges.find((edge) => {
       return (edge.idxA == idx && edge.idxB == lastActive) ||
@@ -82,7 +105,8 @@ function onMouseUp(e: MouseEvent) {
 }
 
 function renderEdges() {
-  ctx.strokeStyle = "#ff8888"
+  ctx.strokeStyle = MAIN_COL
+  ctx.lineWidth = 1;
   edges.forEach(({ idxA, idxB }) => {
     ctx.beginPath()
     ctx.moveTo(nodes[idxA].x, nodes[idxA].y)
@@ -92,7 +116,7 @@ function renderEdges() {
 }
 
 function renderNodes() {
-  ctx.fillStyle = "#ff8888"
+  ctx.fillStyle = MAIN_COL
   nodes.forEach(({ x, y, active }) => {
     ctx.fillRect(x - (RECT_SIZE * 0.5), y - (RECT_SIZE * 0.5), RECT_SIZE, RECT_SIZE)
     if (active) {
@@ -110,7 +134,7 @@ function getAngleOfLine(target: GraphNode, source: GraphNode): AngleRadians {
 }
 
 function renderAngles() {
-  ctx.strokeStyle = "#ff8888"
+  ctx.strokeStyle = MAIN_COL
   const groups = findPivots()
   groups.forEach(({ edges, anchor }) => {
     const sortedEdges = edges.map(edge =>
@@ -138,6 +162,8 @@ function renderAngles() {
       const radius = Math.pow(1 - (angleDiff / (Math.PI * 2)), 2.0) * 24 + 10
       const angleText = `${angleSize.toFixed(1)}°`
 
+      ctx.lineWidth = 1
+      ctx.strokeStyle = MAIN_COL
       ctx.beginPath()
       ctx.arc(nodes[anchor].x, nodes[anchor].y, radius, edgeAngles[edgeAngles.length - 1], edgeAngles[0])
       ctx.stroke()
@@ -148,7 +174,10 @@ function renderAngles() {
       const textX = Math.cos(textAngle) * textRadius + nodes[anchor].x
       const textY = Math.sin(textAngle) * textRadius + nodes[anchor].y
 
+      ctx.strokeStyle = "white"
+      ctx.lineWidth = 0.2
       ctx.fillText(angleText, textX, textY)
+      ctx.strokeText(angleText, textX, textY)
     }
 
     for (let i = 0; i < edgeAngles.length - 1; i++) {
@@ -161,6 +190,8 @@ function renderAngles() {
       const radius = Math.pow(1 - (angleDiff / (Math.PI * 2)), 2.0) * 24 + 10
       const angleText = `${angleSize.toFixed(1)}°`
 
+      ctx.lineWidth = 1
+      ctx.strokeStyle = MAIN_COL
       ctx.beginPath()
       ctx.arc(nodes[anchor].x, nodes[anchor].y, radius, edgeAngles[i], edgeAngles[i + 1])
       ctx.stroke()
@@ -171,7 +202,10 @@ function renderAngles() {
       const textX = Math.cos(textAngle) * textRadius + nodes[anchor].x
       const textY = Math.sin(textAngle) * textRadius + nodes[anchor].y
 
+      ctx.strokeStyle = "white"
       ctx.fillText(angleText, textX, textY)
+      ctx.lineWidth = 0.2
+      ctx.strokeText(angleText, textX, textY)
     }
   })
 }
